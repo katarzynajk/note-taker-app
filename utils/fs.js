6c1eea93cb83a406e163e4015a6e2d01684a7225
Katarzyna Jaromin-Kaliciak <jarominyanni@gmail.com>
2022-07-30 01:55:54 +0100

    trying to fix deploy page

--- utils/fs.js
+++ utils/fs.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const path = require("path");
-
+​
 //const readFromFile = util.promisify(fs.readFile);
-
+​
 const readFromFile = () => {
   try {
     const filePath = path.join(__dirname, `../db/db.json`);
@@ -12,41 +12,35 @@ const readFromFile = () => {
     console.log(`[ERROR: Failed to read data from file | ${error.message}]`);
   }
 };
-
-//write to file
-const writeToFile = (title, text) => {
-  fs.writeFileSync(
-    path.join(__dirname, `../db/${title}.json`),
-    JSON.stringify(text)
+​
+/**
+ *  Writes data to the JSON file.
+ *
+ *  @param {string} destination The file to be written to.
+ *  @param {object} content The content to be written to the file.
+ */
+const writeToFile = (content) => {
+  const filePath = path.join(__dirname, `../db/db.json`);
+  fs.writeFile(filePath, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${filePath}`)
   );
 };
-
-//Writes data to the JSON file.
-
-//@param {string} destination The file to be written to.
-//@param {object} content The content to be written to the file.
-
-/*const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );*/
-
+​
 /**
  *  Reads the data from a given file and appends content.
  *
  *  @param {object} content The content to be appended to the file.
  *  @param {string} file The path to the file to be saved to.
  */
-const readAndAppend = (content, file) => {
-  fs.readFile(file, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
-    }
-  });
+const readAndAppend = (content) => {
+  const filePath = path.join(__dirname, `../db/db.json`);
+  const rawData = fs.readFileSync(filePath, "utf8");
+​
+  if (rawData !== null) {
+    const parsedData = JSON.parse(rawData);
+    parsedData.push(content);
+    writeToFile(parsedData);
+  }
 };
-
+​
 module.exports = { readFromFile, readAndAppend, writeToFile };
